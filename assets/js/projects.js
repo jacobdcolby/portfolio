5fb997abdd81be2aa6b7b5d1e5306999cf5a2030
jacobdcolby <129423182+jacobdcolby@users.noreply.github.com>
2024-03-04 14:56:42 -0600

    File path fix.

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("assets/projects.json")
+    fetch("../../projects.json")
         .then(response => response.json())
         .then(data => {
             const projectsSection = document.getElementById("projects");
